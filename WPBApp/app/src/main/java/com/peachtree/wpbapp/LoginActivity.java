94c9e0be932d164e28816573cb57c1a6a964493b
Tyron18 <Tyron18@hotmail.com>
2016-08-24 00:23:33 +0200

    AppCreation
    Initial app set-up and Login and register layouts first drafts done

---
+++ WPBApp/app/src/main/java/com/peachtree/wpbapp/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.peachtree.wpbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+	}
+}
