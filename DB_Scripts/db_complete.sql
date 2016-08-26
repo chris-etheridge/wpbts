08b8c03ae638fc0b9eecd74be5045d30f0869990
Kyle <kyle-burton@live.co.za>
2016-08-26 11:41:42 +0200

    [db] put all code in to 1 script

--- DB_Scripts/db_complete.sql
+++ DB_Scripts/db_complete.sql
@@ -1,3 +1,88 @@
+drop table if exists TBL_ALERT, 
+TBL_EVENT, 
+TBL_EVENT_TYPE, 
+TBL_ADMIN, 
+TBL_CLINIC, 
+TBL_USER, 
+TBL_ADDRESS;
+
+
+create table TBL_ADDRESS(
+	ADDRESS_ID int primary key,
+	CITY varchar(50),
+	OFFICE varchar(50),
+	STREET varchar(50),
+	AREA varchar(50),
+	AREA_CODE varchar(10),
+	BUILDING_NUMBER int
+);
+
+create table TBL_USER(
+	USER_ID int primary key,
+	FIRST_NAME varchar(20),
+	LAST_NAME varchar(30),
+	NATIONAL_ID char(13),
+	EMAIL varchar(50),
+	PHONE varchar(10),
+	BLOOD_TYPE varchar(3),
+	ADDRESS_ID int,
+	DATE_OF_BIRTH datetime,
+	TITLE varchar(4),
+	GENDER char(1),
+	LANGUAGE_PREF varchar(20),
+	PASSPORT_NUM varchar(20),
+	
+	foreign key (ADDRESS_ID) references TBL_ADDRESS(ADDRESS_ID)
+);
+
+create table TBL_CLINIC(
+	CLINIC_ID int primary key,
+	ADDRESS_ID int,
+	CONTACT_1 varchar(15),
+	CONTACT_2 varchar(15),
+	DESCRIPTION varchar(255),	
+	foreign key (ADDRESS_ID) references TBL_ADDRESS(ADDRESS_ID)
+);
+
+create table TBL_ADMIN(
+	ADMIN_ID int primary key,
+	EMAIL varchar(50),
+	FIRST_NAME varchar(20),
+	LAST_NAME varchar(50),
+	PASSWORD varchar(100)
+);
+
+create table TBL_EVENT_TYPE(
+	TYPE_ID int primary key,
+	DESCRIPTION varchar(255),
+	URGENCY varchar(5)
+);
+
+create table TBL_EVENT(
+	EVENT_ID int primary key,
+	EVENT_DATE datetime,
+	ADDRESS_ID int,
+	TYPE_ID int,
+	DESCRIPTION varchar(255),
+	TITLE varchar(50),
+	ACTIVE boolean,
+	CREATOR_ID int,
+	
+	foreign key (ADDRESS_ID) references TBL_ADDRESS(ADDRESS_ID),
+	foreign key (TYPE_ID) references TBL_EVENT_TYPE(TYPE_ID),
+	foreign key (CREATOR_ID) references TBL_ADMIN(ADMIN_ID)
+);
+
+create table TBL_ALERT(
+	ALERT_ID int primary key,
+	TYPE_ID int,
+	TITLE varchar(50),
+	DESCRIPTION int, /* int refferences enum */
+	
+	foreign key (TYPE_ID) references TBL_EVENT_TYPE(TYPE_ID)
+);
+
+/* inserts */
 /* TBL_ADDRESS */
 INSERT INTO TBL_ADDRESS VALUES( 1, 'Berlin', '6', 'P.O. Box 622, 1849 Eu St.', 'BE', '78059', 6);
 INSERT INTO TBL_ADDRESS VALUES( 2, 'Vico del Gargano', '1', 'P.O. Box 138, 3534 Risus Avenue', 'Puglia', '25737', 4);
@@ -66,4 +151,15 @@ values
 (3,'2015-10-25',3,5,'College outreach and educational','College Outreach',true,3),
 (4,'2016-06-29',4,6,'Corporate Internal administrative meeting','Corporate Private Event',true,4);
 
+/* views */
+CREATE OR REPLACE VIEW VIEW_CLINICSWADDRESS AS
+SELECT TBL_CLINIC.*, CITY, OFFICE, STREET, AREA, AREA_CODE, BUILDING_NUMBER
+FROM TBL_CLINIC
+JOIN TBL_ADDRESS ON TBL_ADDRESS.ADDRESS_ID = TBL_CLINIC.ADDRESS_ID;
+
+CREATE OR REPLACE VIEW VIEW_EVENTSWADDRESS AS
+SELECT TBL_EVENT.*, CITY, OFFICE, STREET, AREA, AREA_CODE, BUILDING_NUMBER, TBL_EVENT_TYPE.DESCRIPTION AS TYPE_DESCRIPTION, URGENCY
+FROM TBL_EVENT
+JOIN TBL_ADDRESS ON TBL_ADDRESS.ADDRESS_ID = TBL_EVENT.ADDRESS_ID
+JOIN TBL_EVENT_TYPE ON TBL_EVENT_TYPE.TYPE_ID = TBL_EVENT.TYPE_ID;
 
