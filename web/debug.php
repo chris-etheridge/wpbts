4d703b34e483a55ebd72a9b0f28bdcb73f79d7ae
Kyle <kyle-burton@live.co.za>
2016-10-16 21:43:06 +0200

    Test commit

--- web/debug.php
+++ web/debug.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Debug</title>
+    <title>Debug - WPBTS</title>
 </head>
 <body>
     <button id="btn-events">Get Events</button>
