d712af64d73526e7dbe3922e61f1695d9af35207
Kyle <kyle-burton@live.co.za>
2016-10-18 17:30:51 +0200

    [API]rearranged folder structure

--- web/api/clinics/view_Clinics.php
+++ web/api/clinics/view_Clinics.php
@@ -1,17 +1,13 @@
 <?php
 
 header("content-type:application/json");
-require_once("../php/DBConn.php");
+require_once("../../php/DBConn.php");
 require_once("functions.php");
 
-$eventid = $mysqli->real_escape_string($_POST['eventid']);
-
-$event = getEvents($mysqli, $eventid);
+$clinics = getClinics($mysqli);
 
 // $_POST['page'] tells us which array of results to load.
 // this can be more complex once you implement a functional database.
-echo json_encode($event);
+echo json_encode($clinics);
 
 exit();
-
-?>
--- web/api/events/view_Events.php
+++ web/api/events/view_Events.php
@@ -1,13 +1,14 @@
 <?php
 
 header("content-type:application/json");
-require_once("../php/DBConn.php");
+require_once("../../php/DBConn.php");
 require_once("functions.php");
 
-$clinics = getClinics($mysqli);
+$events = getEvents($mysqli);
 
 // $_POST['page'] tells us which array of results to load.
 // this can be more complex once you implement a functional database.
-echo json_encode($clinics);
+echo json_encode($events);
 
 exit();
+
