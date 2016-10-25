ec5313b0fc35ef2d0dc775d9a71de4d0d1ccd346
Kyle <kyle-burton@live.co.za>
2016-10-25 19:39:31 +0200

    [WEB] All functions for events (CRUD) are fully functional

---
+++ web/php/cancel-event.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("DBConn.php");
+//require_once("../api/events/functions.php");
+
+session_start();
+
+if(!isset($_GET['eventid']))
+{
+    $_SESSION['alert']['message_type'] = "alert-warning";
+    $_SESSION['alert']['message_title'] = "Warning!";
+    $_SESSION['alert']['message'] = "No event was chosen to be cancelled!";
+    header('Location: ../events.php');
+    exit();
+}
+
+$eventid = $mysqli->real_escape_string($_GET['eventid']);
+
+
+$sql = "UPDATE TBL_EVENT SET ACTIVE = 0 WHERE EVENT_ID = $eventid;";
+$mysqli->query($sql);
+
+if($mysqli->error) //redirect user to edit/create page
+{
+    $_SESSION['alert']['message_type'] = "alert-danger";
+    $_SESSION['alert']['message_title'] = "Error cancelling event!";
+    $_SESSION['alert']['message'] = "Unknown error. If problem persists, contact system administrator!";
+    header('Location: ../events.php');
+    exit();
+}
+
+$_SESSION['alert']['message_type'] = "alert-success";
+$_SESSION['alert']['message_title'] = "SUCCESS!";
+$_SESSION['alert']['message'] = "Event cancelled successfully.";
+
+header('Location: ../events.php');
+exit();
