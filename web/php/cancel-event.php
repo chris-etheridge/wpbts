a742da27c4f79f7ce51858ca214971184ce3ed46
Kyle <kyle-burton@live.co.za>
2016-10-25 23:27:18 +0200

    [WEB] fixed minor bug on event canceling

--- web/php/cancel-event.php
+++ web/php/cancel-event.php
@@ -17,13 +17,16 @@ if(!isset($_GET['eventid']) || !isset($_GET['cancel']))
 $eventid = $mysqli->real_escape_string($_GET['eventid']);
 $status = $mysqli->real_escape_string($_GET['cancel']);
 
+$action = ((int)$status === 0) ? "canceling" : "uncanceling";
+$actionPastTense = ((int)$status === 0) ? "canceled" : "uncanceled";
+
 $sql = "UPDATE TBL_EVENT SET ACTIVE = $status WHERE EVENT_ID = $eventid;";
 $mysqli->query($sql);
 
 if($mysqli->error) //redirect user to edit/create page
 {
     $_SESSION['alert']['message_type'] = "alert-danger";
-    $_SESSION['alert']['message_title'] = "Error cancelling event!";
+    $_SESSION['alert']['message_title'] = "Error $action event!";
     $_SESSION['alert']['message'] = "Unknown error. If problem persists, contact system administrator!";
     header('Location: ../events.php');
     exit();
@@ -31,7 +34,7 @@ if($mysqli->error) //redirect user to edit/create page
 
 $_SESSION['alert']['message_type'] = "alert-success";
 $_SESSION['alert']['message_title'] = "SUCCESS!";
-$_SESSION['alert']['message'] = "Event cancelled successfully.";
+$_SESSION['alert']['message'] = "Event $actionPastTense successfully.";
 
 header('Location: ../events.php');
 exit();
