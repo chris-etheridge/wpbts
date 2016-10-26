434cc0dc4cd2fcb85c813ebd65decfce8697c8c7
David Abes <daveabes@gmail.com>
2016-10-26 16:14:31 +0200

    Working on remove function

--- web/php/form-handler-user-remove.php
+++ web/php/form-handler-user-remove.php
@@ -10,28 +10,4 @@ session_start();
 include("../address_functions.php");
 include("../users_functions.php");
 
-$userIDToRemove = $_GET['userID'];
-$addressIDToRemove = $_GET['addressID'];
-
-
-//lets remove the user and finally, lets remove the address.
-$userDelResult = deleteUser($userIDToRemove);
-//if ($userDelResult == false) {
-//    $_SESSION['alert']['message_type'] = "alert-danger";
-//    $_SESSION['alert']['message_title'] = "Unable to remove user record, check server logs";
-//    header('Location: ' . $_SERVER['HTTP_REFERER']);
-//    exit();
-//}
-////
-//
-//$addrDeleteionResult = removeAddress($addressIDToRemove);
-//if ($addrDeleteionResult == false) {
-//    $_SESSION['alert']['message_type'] = "alert-danger";
-//    $_SESSION['alert']['message_title'] = "Unable to remove address record, check server logs.";
-//    header('Location: ' . $_SERVER['HTTP_REFERER']);
-//    exit();
-//}
-//
-//
-//
-//
+?>
