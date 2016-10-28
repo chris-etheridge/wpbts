b8a47e6c669da4d4ec5a4201b000afc29696d883
DaveAbes <daveabes@gmail.com>
2016-10-28 10:17:19 +0200

    FB Token Registration Complete

--- web/api/firebase/device_token_registration.php
+++ web/api/firebase/device_token_registration.php
@@ -9,6 +9,11 @@
  *
  */
 
+require_once("../../php/DBConn_Dave.php");
+//require_once("functions.php");
+global $dbConn;
+
+
 $receivedData = file_get_contents('php://input');
 
 $jsonString = json_decode($receivedData, true);
@@ -21,4 +26,13 @@ if ($jsonString == null) {
 $userEmail = $jsonString['EMAIL'];
 $userToken = $jsonString['TOKEN'];
 
-$sql = "";
+$sql = "INSERT INTO TBL_DEVICES (DEVICE_EMAIL, DEVICE_TOKEN) VALUES (?,?)";
+$stmt = $dbConn->prepare($sql);
+$stmt->bindParam(1, $userEmail);
+$stmt->bindParam(2, $userToken);
+
+if ($stmt->execute()) {
+    echo "112 - Success, device token registered";
+} else {
+    echo "111 - Error: " . print_r($stmt->errorInfo());
+}
