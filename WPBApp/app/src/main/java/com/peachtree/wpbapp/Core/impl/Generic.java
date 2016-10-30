87559bb23203acc4a08e5b19cffc904b326e1ff4
Chris Etheridge <chris.etheridge@cognician.com>
2016-10-30 12:52:25 +0200

    [APP] Error codes.

--- WPBApp/app/src/main/java/com/peachtree/wpbapp/Core/impl/Generic.java
+++ WPBApp/app/src/main/java/com/peachtree/wpbapp/Core/impl/Generic.java
@@ -12,4 +12,16 @@ public class Generic {
         API_URL = api_url;
     }
 
+
+    public static class ErrorCodes {
+        public static final int RSVP__INSUFFICIENT_PARAMETERS_SUPPLIED = 443;
+        public static final int RSVP__ATTENDING_ILLEGAL_OPTION = 444;
+        public static final int RSVP__UNKNWON_DB_ERROR = 445;
+        public static final int GET_EVENTS__UNKNOWN_DB_ERROR = 222;
+        public static final int GET_EVENTS__NO_ID_SPECIFIED_ERROR = 223;
+        public static final int GET_CLINICS__UNKNOWN_DB_ERROR = 332;
+        public static final int GET_CLINICS__NO_CLINIC_ID_SPECIFIED = 333;
+    }
+
+
 }
