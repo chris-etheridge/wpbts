4787718a4c18dbc01bd63ad0b218b1204dca28f9
Chris Etheridge <chris.etheridge@cognician.com>
2016-10-30 12:28:06 +0200

    [APP] Accounts stub. Not logged in exception.

--- WPBApp/app/src/main/java/com/peachtree/wpbapp/Core/Account.java
+++ WPBApp/app/src/main/java/com/peachtree/wpbapp/Core/Account.java
@@ -1,24 +1,35 @@
 package com.peachtree.wpbapp.Core;
 
 import android.app.Activity;
+import android.util.Log;
 
+import com.peachtree.wpbapp.Core.impl.Generic;
+import com.peachtree.wpbapp.Core.impl.UserNotLoggedInException;
+import com.peachtree.wpbapp.Entities.User;
 import com.peachtree.wpbapp.R;
 
 /**
  * Functions to manage the user account.
  * Default constructor will try log the user in, or get the currently logged in user.
  */
-public class Account extends Activity {
+public class Account extends Generic {
 
-    private String ACCOUNT_API_URL = getResources().getString(R.string.API_ACCOUNT_URL);
+    private User CURRENT_USER = null;
 
-    // when creating an account object, we should try log that user in
-    public Account(String email, String password) {
+    public Account(String api_url, String email, String password) {
+        super(api_url);
+
         LogIn(email, password);
     }
 
     // log a new user in
-    public void LogIn(String email, String password) {}
+    public User LogIn(String email, String password) {
+        if(logged_in_q()) {
+            return CURRENT_USER;
+        } else {
+            return new User();
+        }
+    }
 
     // log the current user out
     public void LogOut(String email) {}
@@ -27,9 +38,26 @@ public class Account extends Activity {
     public void IsLoggedIn(String email) {}
 
     // update the user account profile
-    public void UpdateProfile(String email) {}
+    public void UpdateProfile(String email) throws UserNotLoggedInException {
+        if(logged_in_q()) {
+
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
 
     // register a new user
-    public void Register(String email) {}
+    public User Register(String email) {
+        if(logged_in_q()) {
+            return CURRENT_USER;
+        } else {
+            // create account and log the user in
+            return new User();
+        }
+    }
+
+    private boolean logged_in_q() {
+        return CURRENT_USER != null;
+    }
 
 }
---
+++ WPBApp/app/src/main/java/com/peachtree/wpbapp/Core/impl/UserNotLoggedInException.java
@@ -0,0 +1,9 @@
+package com.peachtree.wpbapp.Core.impl;
+
+/**
+ * Created by chrisetheridge on 10/30/16.
+ */
+
+public class UserNotLoggedInException extends Exception {
+
+}
