65cb1d2d9b1275a56ecd88d3e2cd85e26f37f7f6
Kyle <kyle-burton@live.co.za>
2016-10-31 21:06:11 +0200

    [API] fixed a small bug where a json array with a single element is returned instead of just the json element. May cause issues

--- web/api/clinics/view_Clinic.php
+++ web/api/clinics/view_Clinic.php
@@ -1,27 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 header("content-type:application/json");
 require_once("../../php/DBConn.php");
 require_once("functions.php");
 
-if(!isset($_POST['clinicid']))
+//error - no id posted with request
+if(!isset($_POST['clinicid'])) 
 {
     echo json_encode(array("code" => "333", "message" => "No ID specified"));
     exit();
 }
 
-$clinicid = $mysqli->real_escape_string($_POST['clinicid']);
+$clinicid = $mysqli->real_escape_string($_POST['clinicid']); //filter post
 
 $clinic = getClinic($mysqli, $clinicid);
 
-// $_POST['page'] tells us which array of results to load.
-// this can be more complex once you implement a functional database.
-echo json_encode($clinic);
+echo json_encode($clinic[0]); //return just the first element, a single clinic
 
 exit();
