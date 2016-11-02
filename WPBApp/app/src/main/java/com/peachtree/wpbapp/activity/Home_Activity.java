af9a18a7667747f310ecacedb686e07a8e095066
Tyron18 <Tyron18@hotmail.com>
2016-11-02 21:13:29 +0200

    reload options
    -@Chris, inside the if statement Line 272

--- WPBApp/app/src/main/java/com/peachtree/wpbapp/activity/Home_Activity.java
+++ WPBApp/app/src/main/java/com/peachtree/wpbapp/activity/Home_Activity.java
@@ -19,6 +19,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -253,6 +254,27 @@ public class Home_Activity extends AppCompatActivity implements NavigationView.O
 		});
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.refresh, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.reload) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	public boolean onNavigationItemSelected(@NonNull MenuItem item)
