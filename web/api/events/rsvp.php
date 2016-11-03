87a784914b4b17ea51e963c48472571902eca2c5
Chris Etheridge <chris.etheridge@cognician.com>
2016-11-03 12:00:05 +0200

    RSVP fix

--- web/api/events/rsvp.php
+++ web/api/events/rsvp.php
@@ -4,16 +4,19 @@ header("content-type:application/json");
 require_once("../../php/DBConn.php");
 require_once("functions.php");
 
-if(!isset($_POST['eventid']) || !isset($_POST['attending']) || !isset($_POST['userid'])) //error where something crucial was not posted
-{
+$receivedData = file_get_contents('php://input');
+$jsonData = json_decode($receivedData, true);
+
+if(!isset($jsonData['eventid']) || !isset($jsonData['attending']) || !isset($jsonData['userid'])) //error where something crucial was not posted
+{    
     echo json_encode(array("code" => "443", "message" => "Not all values supplised. Required values are eventid(int), attending (int - 1 or 0), userid (int)."));
     exit();
 }
 
 //filtering post values
-$eventid = (int)$mysqli->real_escape_string($_POST['eventid']);
-$attending = (int)$mysqli->real_escape_string($_POST['attending']);
-$userid = (int)$mysqli->real_escape_string($_POST['userid']);
+$eventid = (int)$mysqli->real_escape_string($jsonData['eventid']);
+$attending = (int)$mysqli->real_escape_string($jsonData['attending']);
+$userid = (int)$mysqli->real_escape_string($jsonData['userid']);
 
 if($attending !== 1 && $attending !== 0) //error where illegal option was posted. must be 1 or 0
 {
